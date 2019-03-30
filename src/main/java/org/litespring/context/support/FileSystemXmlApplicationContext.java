cabb7378fa2f0bdfba78998d87d2508c3145dfd2
css12345 <2318018219@qq.com>
2019-03-30 23:15:03 +0800

    Revert "Revert "把类名FileSystemApplicationContext修改为FileSystemXmlApplicationContext""
    This reverts commit b68b5f679e662c34b01843faf65eac4356da23fc.

--- src/main/java/org/litespring/context/support/FileSystemXmlApplicationContext.java
+++ src/main/java/org/litespring/context/support/FileSystemXmlApplicationContext.java
@@ -3,8 +3,8 @@ package org.litespring.context.support;
 import org.litespring.core.io.FileSystemResource;
 import org.litespring.core.io.Resource;
 
-public class FileSystemApplicationContext extends AbstractApplicationContext {
-	public FileSystemApplicationContext(String filePath) {
+public class FileSystemXmlApplicationContext extends AbstractApplicationContext {
+	public FileSystemXmlApplicationContext(String filePath) {
 		super(filePath);
 	}
 
